fc04c043d6b3f6b60d1b3d440ec8fcd4e4be2aff
John Wick <72218928+johnsoul777@users.noreply.github.com>
2020-12-21 03:45:42 +0100

    Important Fix
    Fix, excessive memory consumption

--- client/main.lua
+++ client/main.lua
@@ -18,7 +18,7 @@ ESX = nil
 
 Citizen.CreateThread(function()
 	while ESX == nil do
-		TriggerEvent('esx:esx:getSharedObject', function(obj) ESX = obj end)
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
 	end
 
